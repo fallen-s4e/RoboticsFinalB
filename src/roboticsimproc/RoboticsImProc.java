75a418098a43ad787739b4fdf7940f24388a4e40
fallen <fallen.s4e@gmail.com>
2013-02-23 12:45:12 +0600

    init commit

---
+++ src/roboticsimproc/RoboticsImProc.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package roboticsimproc;
+
+/**
+ *
+ * @author fallen
+ */
+public class RoboticsImProc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
