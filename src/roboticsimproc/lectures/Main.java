9d1d14f313e8468597e74d1c0bd3e3ef13e8fe09
fallen <fallen.s4e@gmail.com>
2013-02-24 12:04:01 +0600

    sizes fixed

--- src/roboticsimproc/lectures/Main.java
+++ src/roboticsimproc/lectures/Main.java
@@ -4,6 +4,7 @@
  */
 package roboticsimproc.lectures;
 
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 
@@ -18,12 +19,16 @@ public class Main {
             @Override
             public void run() {
                 JFrame frame = new JFrame();
-                
                 CImage ci = new CImage("trackPhotos/foto1.jpg");
+                Dimension d = new Dimension(ci.getW()+20, ci.getH()+40);
+                
                 PanelDraw pd = new PanelDraw(ci);
+                pd.setPreferredSize(d);
                 
                 frame.getContentPane().add(pd);
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setPreferredSize(d);
+                frame.setSize(d);
                 frame.setVisible(true);
             }
         });
