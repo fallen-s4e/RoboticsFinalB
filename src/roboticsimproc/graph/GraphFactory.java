7056bcfbcbaac5933d76094c0900dd97527ffe05
fallen <fallen.s4e@gmail.com>
2013-02-25 13:09:22 +0600

    performance of makeSparseGraphBestKNeighbours fixed a bit

--- src/roboticsimproc/graph/GraphFactory.java
+++ src/roboticsimproc/graph/GraphFactory.java
@@ -39,14 +39,14 @@ public class GraphFactory {
 
     public IGraph<Point> makeSparseGraphBestKNeighbours(Vector<Point> points,
             int w, int h, int k) {
-        return makeSparseGraphBestKNeighbours(points, w, h, k, null);
+        return makeSparseGraphBestKNeighbours(points, w, h, k, null, 99999);
     }
 
     /**
      * k should not be large affects performance linearly
      */
     public IGraph<Point> makeSparseGraphBestKNeighbours(Vector<Point> points,
-            int w, int h, int k, boolean[][] theshold) {
+            int w, int h, int k, boolean[][] theshold, int maxRadius) {
         GraphSparse<Point> gr = new GraphSparse<Point>();
         for (int i = 0; i < points.size(); i++) {
             Vector<Point> bestNeighbours = new Vector<Point>();
@@ -56,7 +56,8 @@ public class GraphFactory {
                     Point c2 = points.get(j);
                     if (bestNeighbours.size() < k) {
                         if (theshold == null || // no threshold supported
-                                (!ImProcUtils.isObstacleBetweenPoints(c1, c2, theshold))) {
+                                ((ImProcUtils.euclideanDistance(c1, c2) < maxRadius)
+                                && (!ImProcUtils.isObstacleBetweenPoints(c1, c2, theshold)))) {
                             bestNeighbours.add(c2);
                         }
                     } else {
@@ -65,7 +66,8 @@ public class GraphFactory {
                             if (ImProcUtils.euclideanDistance(c1, c2)
                                     < ImProcUtils.euclideanDistance(c1, bestNeighbours.get(l))) {
                                 if (theshold == null
-                                        || !ImProcUtils.isObstacleBetweenPoints(c1, c2, theshold)) {
+                                        || ((ImProcUtils.euclideanDistance(c1, c2) < maxRadius)
+                                        && !ImProcUtils.isObstacleBetweenPoints(c1, c2, theshold))) {
                                     bestNeighbours.set(l, c2);
                                     break;
                                 }
