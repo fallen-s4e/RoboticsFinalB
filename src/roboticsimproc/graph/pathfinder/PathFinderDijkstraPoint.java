b2bbc3efdce9e1bae82912dc2b301349dcd5c016
fallen <fallen.s4e@gmail.com>
2013-02-27 20:23:50 +0600

    dijkstra algo added

--- src/roboticsimproc/graph/pathfinder/PathFinderDijkstraPoint.java
+++ src/roboticsimproc/graph/pathfinder/PathFinderDijkstraPoint.java
@@ -5,7 +5,7 @@
 package roboticsimproc.graph.pathfinder;
 
 import java.awt.Point;
-import java.util.Vector;
+import java.util.*;
 import roboticsimproc.ImProcUtils;
 import roboticsimproc.graph.IGraph;
 
@@ -16,7 +16,78 @@ import roboticsimproc.graph.IGraph;
 public class PathFinderDijkstraPoint implements IPathFinder<Point> {
 
     @Override
-    public Vector<Point> findPath(IGraph<Point> gr, Point startingNode, Vector<Point> osbt) {
+    public Vector<Point> findPath(final IGraph<Point> gr, Point startingNode, Vector<Point> osbt) {
+        Map<Point, Double> bestPriceUnvis = new HashMap();
+        Map<Point, Point> bestPath = new HashMap();
+        Map<Point, Double> visited = new HashMap(); // visited
+        
+        Vector<Point> allNodes = gr.listNodes();
+        bestPriceUnvis.put(startingNode, 0.0);
+        while(visited.size() < allNodes.size()) { // while all is not visited
+            System.out.println("1");
+            // find best one
+            if (!bestPriceUnvis.keySet().iterator().hasNext()) {
+                break;
+            }
+            Point curPointV = bestPriceUnvis.keySet().iterator().next();
+            for (Map.Entry<Point, Double> entry : bestPriceUnvis.entrySet()) {
+                Point k = entry.getKey();
+                if (bestPriceUnvis.get(k) < bestPriceUnvis.get(curPointV)) {
+                    curPointV = k;
+                }
+            }
+            final Point curPoint = curPointV;
+            
+            // not visited neighbours
+            Vector<Point> neighbours = gr.relatedWith(curPoint);
+            for (int i = 0; i < neighbours.size(); i++) {
+                if (visited.containsKey(neighbours.get(i))) {
+                    neighbours.remove(i);
+                    --i;
+                }
+            }
+            
+            Collections.sort(neighbours, new Comparator<Point>() {
+
+                @Override
+                public int compare(Point p1, Point p2) {
+                    return (int) (gr.relationPrice(curPoint, p2) - 
+                            gr.relationPrice(curPoint, p1));
+                }
+            });
+            
+            for (Point neigh : neighbours) {
+                Double oldPrice = bestPriceUnvis.get(neigh);
+                double newPrice = bestPriceUnvis.get(curPoint) + gr.relationPrice(curPoint, neigh);
+                if (oldPrice==null || newPrice < oldPrice) {
+                    bestPriceUnvis.put(neigh, newPrice);
+                    bestPath.put(neigh, curPoint);
+                }
+            }
+            visited.put(curPoint, bestPriceUnvis.get(curPoint));
+            bestPriceUnvis.remove(curPoint);
+        }
+        
+        // farest Node
+        Point p = visited.keySet().iterator().next();
+        for (Map.Entry<Point, Double> entry : visited.entrySet()) {
+            Point k = entry.getKey();
+            Double v = entry.getValue();
+            if (visited.get(k) > visited.get(p)) {
+                p = k;
+            }
+        }
+        Vector<Point> res = new Vector<Point>();
+        while (p != startingNode) {
+            res.add(p);
+            p = bestPath.get(p);
+        }
+        
+        System.out.println("res.size() = " + res.size());
+        return res;
+    }
+    
+    public Vector<Point> findPath1(IGraph<Point> gr, Point startingNode, Vector<Point> osbt) {
         int k = 10;
         Vector<Point> res = new Vector<Point>();
         Point curNode = startingNode;
